030b5231d369776793a17c9769a5b4f6b8c6ec79
NOHI <thisisnohi@163.com>
2022-07-25 13:22:05 +0800

    feat 修改swagger2 -> knife4j

--- ms-eureka/src/main/java/nohi/ms/EurekaApplication.java
+++ ms-eureka/src/main/java/nohi/ms/EurekaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  * 2022-07-11 12:59
  **/
 @SpringBootApplication
-@EnableEurekaServer // 开启 Eureka server,接受其他微服务的注册
+@EnableEurekaServer
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
