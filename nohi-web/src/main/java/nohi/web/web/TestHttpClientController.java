ce8c43b3d843c012e38592d9d2d907bd7928ef39
NOHI <thisisnohi@163.com>
2022-09-17 13:33:38 +0800

    feat 增加测试httpclient服务，记录耗时

--- nohi-web/src/main/java/nohi/web/web/TestHttpClientController.java
+++ nohi-web/src/main/java/nohi/web/web/TestHttpClientController.java
@@ -1,11 +1,14 @@
 package nohi.web.web;
 
+import com.alibaba.fastjson2.JSONObject;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import nohi.common.config.BizConfig;
 import nohi.socket.njserver.NjSocketService;
 import nohi.socket.zshserver.SoketService;
+import nohi.web.dto.httpclient.HttpClientReq;
+import nohi.web.dto.httpclient.HttpClientResp;
 import nohi.web.utils.HttpClientPoolUtils;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.springframework.beans.factory.InitializingBean;
@@ -13,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -34,9 +39,38 @@ public class TestHttpClientController {
         log.info("INFO");
         log.warn("warn");
         log.error("error");
-        String url = "http://www.baidu.com";
+        String url = "http://127.0.0.1:8888/mock/http?sleep=100";
         String msg = HttpClientPoolUtils.doGetRequest(client, url);
         log.debug("msg:" + msg);
         return msg;
+    }
+
+    @ApiOperation("postHttpClient")
+    @PostMapping(value = {"postHttpClient"})
+    public HttpClientResp postHttpClient(@RequestBody HttpClientReq req) {
+        String title = req.getTraceId();
+        long start = System.currentTimeMillis();
+        HttpClientResp resp = null;
+
+        log.info("{} 请求:{}", title, JSONObject.toJSONString(req));
+
+        try {
+            String url = "http://127.0.0.1:8888/mock/http?sleep=100";
+            String reqMsg = JSONObject.toJSONString(req);
+            String msg = HttpClientPoolUtils.post(url, reqMsg);
+            log.debug("msg:" + msg);
+            resp = JSONObject.parseObject(msg, HttpClientResp.class);
+        } catch (Exception e) {
+            log.error("{} 异常:{}", title, e.getMessage());
+            resp = new HttpClientResp();
+            resp.setRetCode(HttpClientResp.RET_FAIL);
+            resp.setRetMsg(e.getMessage());
+        } finally {
+            resp.setTraceId(req.getTraceId());
+            resp.setTxCode(req.getTxCode());
+            long end = System.currentTimeMillis();
+            log.info("{} 耗时[{}]", title, end - start);
+        }
+        return resp;
     }
 }
