7d49097f424a5864f79b422c02e011bff0234f50
NOHI <thisisnohi@163.com>
2022-10-24 17:48:49 +0800

    feat 增加SpringBootEvent

---
+++ nohi-web/src/main/java/nohi/web/service/enent/DemoEventPublisher.java
@@ -0,0 +1,29 @@
+package nohi.web.service.enent;
+
+import nohi.web.event.DemoEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * <h3>nohi-web</h3>
+ *
+ * @author NOHI
+ * @description <p>DemoPublisher</p>
+ * @date 2022/10/24 16:14
+ **/
+@Component
+public class DemoEventPublisher {
+
+    private final ApplicationContext applicationContext;
+
+    @Autowired
+    public DemoEventPublisher(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    public void publish(long id, String message) {
+        applicationContext.publishEvent(new DemoEvent(this, id, message));
+    }
+
+}
