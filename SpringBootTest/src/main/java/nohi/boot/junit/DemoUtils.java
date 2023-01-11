a9125c389246ff78cde3ad9b30b5c7e4e400db5a
NOHI <thisisnohi@163.com>
2023-01-11 13:24:06 +0800

    feat add junit

---
+++ SpringBootTest/src/main/java/nohi/boot/junit/DemoUtils.java
@@ -0,0 +1,15 @@
+package nohi.boot.junit;
+
+/**
+ * <h3>SpringBootTest</h3>
+ *
+ * @author NOHI
+ * @description <p>单元测试</p>
+ * @date 2023/01/10 21:43
+ **/
+public class DemoUtils {
+
+    public int intAdd(int a, int b){
+        return a + b;
+    }
+}
---
+++ SpringBootTest/src/test/java/nohi/boot/springboottest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package nohi.boot.springboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
