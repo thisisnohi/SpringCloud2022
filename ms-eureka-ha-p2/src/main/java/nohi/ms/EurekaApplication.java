57dc7c6a71485332e7715cca611607f8be7bbbdd
NOHI <thisisnohi@163.com>
2023-03-30 12:44:48 +0800

    feat 增加eureak ha

---
+++ ms-eureka-ha-p2/src/main/java/nohi/ms/EurekaApplication.java
@@ -0,0 +1,17 @@
+package nohi.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author NOHI
+ * 2022-07-11 12:59
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
