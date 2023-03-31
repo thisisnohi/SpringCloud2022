d6c829fc10f62bdf148ebb0931602fcc6bebe66b
NOHI <thisisnohi@163.com>
2023-03-31 13:43:54 +0800

    feat 删除所有@EnableEurekaClient

--- ms-config/src/main/java/nohi/ms/ConfigApplication.java
+++ ms-config/src/main/java/nohi/ms/ConfigApplication.java
@@ -3,7 +3,6 @@ package nohi.ms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Gateway
@@ -11,7 +10,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * 2022-07-11 12:59
  **/
 @SpringBootApplication
-@EnableEurekaClient
 @EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
