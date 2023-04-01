8e6450947f9c71e5f5bdc97bfc3c4829eb0ed984
NOHI <thisisnohi@163.com>
2023-04-01 11:48:39 +0800

    k8s eureka

---
+++ ms-eureka/src/test/java/nohi/ms/EurekaApplicationTests.java
@@ -0,0 +1,11 @@
+package nohi.ms;
+
+import org.junit.jupiter.api.Test;
+
+public class EurekaApplicationTests {
+
+    @Test
+    public void testMain() {
+        System.out.println("OK");
+    }
+}
