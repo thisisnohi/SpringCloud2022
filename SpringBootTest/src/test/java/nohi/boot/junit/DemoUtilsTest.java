c9fea0ca572597cc3cc4f48fadf25edf984f402c
NOHI <thisisnohi@163.com>
2023-05-08 14:27:25 +0800

    feat 增加junit 生命周期、displayname

--- SpringBootTest/src/test/java/nohi/boot/junit/DemoUtilsTest.java
+++ SpringBootTest/src/test/java/nohi/boot/junit/DemoUtilsTest.java
@@ -1,9 +1,15 @@
 package nohi.boot.junit;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import lombok.CustomLog;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 
 /**
@@ -13,19 +19,33 @@ import org.junit.jupiter.api.Test;
  * @description <p>基础单元测试内容</p>
  * @date 2023/01/10 21:45
  **/
+@Slf4j
+@DisplayNameGeneration(DisplayNameGenerator.Simple.class)
+@DisplayName("DemoUtilsTest DEMO")
 public class DemoUtilsTest {
 
+    @BeforeAll
+    static void beforeAll() {
+        log.info("==beforeAll===");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        log.info("==afterAll===");
+    }
+
     @BeforeEach
-    void setUp() {
-        System.out.println("setUp");
+    void beforeEach() {
+        log.info("==beforeEach===");
     }
 
     @AfterEach
-    void tearDown() {
-        System.out.println("tearDown");
+    void afterEach() {
+        log.info("==afterEach===");
     }
 
     @Test
+    @DisplayName("测试加法")
     public void testEqualsAndNotEquals() {
         // set up
         DemoUtils demoUtils = new DemoUtils();
@@ -37,10 +57,26 @@ public class DemoUtilsTest {
     }
 
     /**
+     * 失败测试
+     */
+    @Test
+    @DisplayName("测试加法失败")
+    public void testEqualsAndNotEqualsFail() {
+        // set up
+        DemoUtils demoUtils = new DemoUtils();
+        int expected = 6;
+        // execute
+        int actual = demoUtils.intAdd(2, 3);
+        // assert
+        Assertions.assertEquals(expected, actual, "2+4 must be 6");
+    }
+
+    /**
      * 检查对你是否为空
      */
     @Test
-    public void testAssertNull(){
+    @DisplayName("测试对象为空")
+    public void testAssertNull() {
         // set up
         DemoUtils demoUtils = new DemoUtils();
         String str1 = null;
@@ -51,11 +87,15 @@ public class DemoUtilsTest {
         Assertions.assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
 
-    @Test
-    void intAdd() {
-        DemoUtils u = new DemoUtils();
-
-        Assertions.assertEquals(6, u.intAdd(1, 5), "1+5 must equals 6");
-        Assertions.assertNotEquals(5, u.intAdd(1, 5), "1+5 must not equals 5");
+    @ParameterizedTest(name = "#{index} - Test with {0} and {1}")
+    @MethodSource("argumentProvider")
+    void test_method_multi(String str, int length) {
+        log.info("str[{}], length[{}]", str, length);
+    }
+    static Stream<Arguments> argumentProvider() {
+        return Stream.of(
+                arguments("abc", 3),
+                arguments("lemon", 2)
+        );
     }
 }
