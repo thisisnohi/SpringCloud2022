569ba0611d6bbe4d9398d02b0a167b774bc29b98
NOHI <thisisnohi@163.com>
2025-06-16 21:50:18 +0800

    update 升级jar解决漏洞

--- SpringBoot-2-Native/src/main/java/nohi/web/service/proxy/MessagerSendService.java
+++ SpringBoot-2-Native/src/main/java/nohi/web/service/proxy/MessagerSendService.java
@@ -1,13 +1,13 @@
 package nohi.web.service.proxy;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
--- SpringBoot-2-Native/src/main/java/nohi/web/service/proxy/MockSendService.java
+++ SpringBoot-2-Native/src/main/java/nohi/web/service/proxy/MockSendService.java
@@ -1,6 +1,8 @@
 package nohi.web.service.proxy;
 
 import com.alibaba.fastjson2.JSONObject;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import nohi.common.config.BizConfig;
 import nohi.web.utils.FileUtils;
 import org.apache.commons.lang3.StringUtils;
@@ -9,8 +11,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.math.BigDecimal;
 import java.util.Enumeration;
